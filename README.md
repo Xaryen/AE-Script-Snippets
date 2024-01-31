98b39ca0e73ddc33b06f1886b8315af252f40de6
Xaryen <22774446+Xaryen@users.noreply.github.com>
2024-01-31 08:03:00 +0100

    add new script

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # A collection of small AE scripts for animation production.
 
-## importFolder:
+## Standalone scripts:
+
+### importFolder:
 Imports a folder and all it's contents into an AE project, recreates the folder structure in the project panel.  
 For subfolders starting with "_" contents will be imported as individual images, for rest as img sequences.
 
-## sortLayers:
+### sortLayers:
 Sorts layers so that camera/layout is on top, bg on bottom and cels inbetween sorted in correct reverse alphabetical order.
+
+## Functions:
+
+### validatePathWithRegex
+used to check if the path of a file/folder exists and make sure it's the newest revision
---
+++ validatePathWithRegex.jsx
@@ -0,0 +1,66 @@
+/**
+ * checks if a path to a folder/file is valid, find the most recent retake version if present (based on _r appended to the name, e.g. TITLE01_001_paint_rr for a second retake)
+ * @param {string} basePath - the base path where the item e.g. a cut folder or a bg file will be located
+ * @param {string} regexPattern - the naming scheme used for the give work, the suffix like _paint or _BG or a file extension isn't needed
+ * @param {string} inputType - either "file" or "folder"
+ * @returns {string|null} if the path is valid it returns the full path to the file or folder, otherwise returns null
+ */
+(function(){
+
+function validatePathWithRegex(basePath, regexPattern, inputType) {
+    var base = new Folder(basePath)
+    var maxRetakeCount = -1; // Set to -1 so items with no 'r's can be considered
+    var verifiedPath = null;
+    var isFolder = inputType === "folder" || (!inputType && base instanceof Folder);
+
+    if (base.exists) {
+        var items = base instanceof Folder ? base.getFiles() : [base];
+
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            var isItemFolder = item instanceof Folder;
+            var isItemTypeMatched = isFolder ? isItemFolder : !isItemFolder;
+            var valRegex = new RegExp(regexPattern, "i");
+
+            if (isItemTypeMatched && valRegex.test(item.name)) {
+                var match = item.name.match(/_r+/gi) || [];
+                var rCount = match.join('').length - match.length; 
+
+                if (rCount >= maxRetakeCount) {
+                    maxRetakeCount = rCount;
+                    verifiedPath = item;
+                }
+            }
+        }
+    }
+
+    if (!verifiedPath) {
+        verifiedPath = isFolder ? base.selectDlg("No matching folder found. Please select a folder.") :
+                                  base.openDlg("No matching file found. Please select a file.");
+    }
+
+    return verifiedPath ? verifiedPath.fsName : null;
+}
+
+RegExp.escape = function(s) {
+    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+
+// // Example usage
+
+// var basePath = "E:/WORKTITLE/EPISODE 01/0-3_CEL_FOLDER/";
+// var testVar = "TITLE01_C001";
+// var regexPattern = "^" + RegExp.escape(testVar) + "_r*"; // 
+// var type = "folder"; // Can be "folder", "file", or undefined
+// var path = validatePathWithRegex(basePath, regexPattern, type);
+
+
+var basePath = "E:/WORKTITLE/EPISODE 01/0-1_BG/";
+var testVar = "TITLE01_C001";
+var regexPattern = "^" + RegExp.escape(testVar); //
+var type = "file"; // Can be "folder", "file", or undefined
+var path = validatePathWithRegex(basePath, regexPattern, type);
+$.writeln("Selected item: " + path);
+
+}());
+
